6a32d23be5abdcbc1cc67b981a181a72de60ebaa
Rohit Goswami <rog32@hi.is>
2023-08-08 20:16:04 +0000

    MAINT: Add a direct gw driver

---
+++ app/gjp_quad_gw.f90
@@ -0,0 +1,49 @@
+program gjp_quad_gw
+
+use GaussJacobiQuad, only: gauss_jacobi
+use gjp_types, only: dp
+implicit none
+
+integer :: n_points
+real(dp) :: alpha, beta
+real(dp), dimension(:), allocatable :: x, w
+character(len=128) :: arg
+character(len=:), allocatable :: method
+integer :: idx, ierr
+
+if (command_argument_count() /= 3) then
+    print*,"Usage: ./gjp_quad_gw <n_points> <alpha> <beta>"
+    print*,"  n_points: Number of quadrature points (integer)"
+    print*,"  alpha: Parameter alpha for Gauss-Jacobi quadrature (must be > -1)"
+    print*,"  beta: Parameter beta for Gauss-Jacobi quadrature (must be > -1)"
+    error stop "Must supply 3 arguments"
+end if
+
+call get_command_argument(1, arg)
+read (arg, '(i4)') n_points
+allocate (x(n_points), w(n_points))
+
+call get_command_argument(2, arg)
+if (index(arg, '.') == 0) then
+    print*,"alpha must include a decimal point"
+    error stop
+end if
+read (arg, *) alpha
+
+call get_command_argument(3, arg)
+if (index(arg, '.') == 0) then
+    print*,"beta must include a decimal point"
+    error stop
+end if
+read (arg, *) beta
+
+method = "gw"
+call gauss_jacobi(n_points, alpha, beta, x, w, method)
+
+do idx = 1, n_points
+    print '(1X, A, 1P, E24.17, 2X, A, 1P, E23.17)', 'Root: ', x(idx), 'Weight: ', w(idx)
+end do
+
+deallocate (x, w)
+
+end program gjp_quad_gw
