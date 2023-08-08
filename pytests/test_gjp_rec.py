4a52b4ac601dca228fe797676ca4d3c40d0bdf7b
Rohit Goswami <rog32@hi.is>
2023-08-08 00:39:56 +0000

    TST: Add basic pytest logic

---
+++ pytests/test_gjp_rec.py
@@ -0,0 +1,62 @@
+import subprocess
+
+import numpy as np
+import pytest
+
+
+def extract_values(output_str):
+    lines = output_str.strip().split("\n")
+    roots = []
+    weights = []
+    for line in lines:
+        _, root, _, weight = line.split()
+        roots.append(float(root))
+        weights.append(float(weight))
+    return np.array(roots), np.array(weights)
+
+
+def run_fortran(n, alpha, beta):
+    result = subprocess.run(
+        ["fpm", "run", "gjp_quad_rec", "--", str(n), str(alpha), str(beta)],
+        stdout=subprocess.PIPE,
+    )
+    return extract_values(result.stdout.decode())
+
+
+def run_python(n, alpha, beta):
+    result = subprocess.run(
+        [
+            "python",
+            "scripts/gen_analytic_vals.py",
+            "--npts",
+            str(n),
+            "--alpha",
+            str(alpha),
+            "--beta",
+            str(beta),
+        ],
+        stdout=subprocess.PIPE,
+    )
+    return extract_values(result.stdout.decode())
+
+
+@pytest.mark.parametrize(
+    "n, alpha, beta",
+    [
+        (3, 1.0, 5.0),
+        (5, 2.0, 3.0),
+        pytest.param(
+            10,
+            0.0,
+            30.0,
+            marks=pytest.mark.xfail(reason="High beta values diverge"),
+            id="highbeta_fail",
+        ),
+    ],
+)
+def test_gjp_quad_rec(n, alpha, beta):
+    fortran_roots, fortran_weights = run_fortran(n, alpha, beta)
+    python_roots, python_weights = run_python(n, alpha, beta)
+
+    assert np.allclose(fortran_roots, python_roots, atol=1e-14)
+    assert np.allclose(fortran_weights, python_weights, atol=1e-14)
