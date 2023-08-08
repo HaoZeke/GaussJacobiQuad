356a0a5d0523851cf0287b1c5aa6554d1c1ac92f
Rohit Goswami <rog32@hi.is>
2023-08-08 10:46:42 +0000

    ENH: Check to ensure real values

--- src/GaussJacobiQuad.f90
+++ src/GaussJacobiQuad.f90
@@ -2,7 +2,7 @@
 !
 !! @details This module implements the Gauss-Jacobi quadrature method for numerical integration.
 !! It provides a subroutine for obtaining the weights and nodes, which dispatches to multiple implementations
-!! based on the method provided. Available methods include "recurrence" and other potential implementations.
+!! based on the method provided. Available methods include "recurrence".
 module GaussJacobiQuad
 use gjp_rec, only: gauss_jacobi_rec
 use gjp_types, only: dp
@@ -15,11 +15,22 @@ subroutine gauss_jacobi(n, a, b, x, w, method)
     real(dp), intent(out) :: x(n), w(n)
     character(len=:), allocatable, intent(in) :: method
 
+    if (a <= -1.0_dp) then
+        print*,"Error: alpha must be greater than -1"
+        error stop
+    end if
+
+    if (b <= -1.0_dp) then
+        print*,"Error: beta must be greater than -1"
+        error stop
+    end if
+
     select case (trim(method))
     case ("recurrence")
         call gauss_jacobi_rec(n, a, b, x, w)
     case default
-        print*,"Error: Unknown method specified: ", method
+        print*,"Error: Unknown method specified:", method
+        print*,"Supported methods: 'recurrence'"
         error stop
     end select
 end subroutine gauss_jacobi
