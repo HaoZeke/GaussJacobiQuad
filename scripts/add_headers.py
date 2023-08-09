0e6a49a8c6ed0a72a909bbab77d76b311ad599ae
Rohit Goswami <rog32@hi.is>
2023-08-09 00:38:18 +0000

    ENH: Rework the header into python

---
+++ scripts/add_headers.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+
+import os
+import re
+
+from jinja2 import Template
+
+# Get the Git root directory
+GITROOT = os.popen("git rev-parse --show-toplevel").read().strip()
+
+# Define the header template using Jinja2
+header_template = Template(
+    """! BEGIN_HEADER
+! -----------------------------------------------------------------------------
+! Gauss-Jacobi Quadrature Implementation
+! Authors: {{ author }}
+! Source: {{ library_name }} Library
+! License: MIT
+! GitHub Repository: {{ repository_url }}
+! -----------------------------------------------------------------------------
+! This code is part of the {{ library_name }} library, providing an efficient
+! implementation for Gauss-Jacobi quadrature nodes and weights computation.
+! -----------------------------------------------------------------------------
+! END_HEADER
+
+"""
+)
+
+# Fill in the template variables
+header_text = header_template.render(
+    author="Rohit Goswami <rgoswami[at]ieee.org>",
+    repository_url="https://github.com/HaoZeke/GaussJacobiQuad",
+    library_name="GaussJacobiQuad",
+)
+
+# Iterate over all Fortran files in the SRC directory
+for root, dirs, files in os.walk(os.path.join(GITROOT, "src")):
+    for filename in files:
+        if filename.endswith(".f90"):
+            filepath = os.path.join(root, filename)
+
+            with open(filepath, "r") as file:
+                content = file.read()
+
+            # Check for the existing header and replace or append
+            if "BEGIN_HEADER" in content and "END_HEADER" in content:
+                content = re.sub(
+                    r"! BEGIN_HEADER.*?! END_HEADER\n",
+                    header_text,
+                    content,
+                    flags=re.DOTALL,
+                )
+            else:
+                content = header_text + content
+
+            with open(filepath, "w") as file:
+                file.write(content)
+
+print("Headers added or updated in all Fortran files in {}/src.".format(GITROOT))
