ed86655e78c3ee08e5819d57d15df6f1f7638911
Rohit Goswami <rog32@hi.is>
2023-08-25 23:42:31 +0000

    ENH: Add a CLI to the python library

---
+++ interfaces/PyInterface/gjquad_cli.py
@@ -0,0 +1,36 @@
+import argparse
+
+import gjquadpy
+
+
+def main(n, alpha, beta, meth):
+    if meth == "recurrence":
+        roots, weights = gjquadpy.gaussjacobiquadccompat.gauss_jacobi_rec_c(
+            n, alpha, beta
+        )
+    elif meth == "gw":
+        roots, weights = gjquadpy.gaussjacobiquadccompat.gauss_jacobi_gw_c(
+            n, alpha, beta
+        )
+    # print(f"method: {meth}, npts: {n}, alpha: {alpha}, beta: {beta}, range: [-1, 1]")
+    for idx, root in enumerate(roots):
+        sign = " " if root >= 0 else ""
+        root_str = f"{root:23.17E}"
+        weight_str = f"{weights[idx]:23.17E}"
+        print(f"Root:{sign} {root_str} Weight: {weight_str}")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Compute Gauss-Jacobi quadrature via GaussJacobiQuad."
+    )
+    parser.add_argument("--npts", type=int, default=5, help="Degree of the polynomial.")
+    parser.add_argument("--alpha", type=float, default=0, help="Alpha parameter.")
+    parser.add_argument("--beta", type=float, default=12, help="Beta parameter.")
+    parser.add_argument(
+        "--meth", type=str, default="gw", help="Method.", choices=["gw", "recurrence"]
+    )
+
+    args = parser.parse_args()
+
+    main(args.npts, args.alpha, args.beta, args.meth)
