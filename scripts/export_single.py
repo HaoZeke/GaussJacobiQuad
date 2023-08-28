a98280daf9084616a92231b060fd25970b2c7420
Rohit Goswami <rog32@hi.is>
2023-08-28 21:58:15 +0000

    ENH: Add a single file exporter

---
+++ scripts/export_single.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+# Cannot use ! in statements with this if strip-comments is true
+
+import argparse
+import os
+import re
+import subprocess
+from pathlib import Path
+
+import fortdepend
+
+
+def get_git_root():
+    try:
+        git_root = (
+            subprocess.check_output(["git", "rev-parse", "--show-toplevel"])
+            .strip()
+            .decode("utf-8")
+        )
+        return Path(git_root)
+    except subprocess.CalledProcessError:
+        print("This directory is not part of a Git repository.")
+        return None
+
+
+def process_files(fname, strip_comments=True):
+    with open(fname, "r") as f:
+        content = f.read()
+    # Remove everything between ! BEGIN_HEADER and ! END_HEADER
+    content = re.sub(r"! BEGIN_HEADER.*?! END_HEADER\n", "", content, flags=re.DOTALL)
+    # Optionally, remove lines starting with !
+    if strip_comments:
+        content = re.sub(r"(!.*?$)|(\s*!.*?$)", "", content, flags=re.MULTILINE)
+    return content
+
+
+def export_single(modulename, strip_comments=True, outname=None):
+    if outname is None:
+        outname = f"{modulename}_single.f90"
+
+    prj_srcs = Path(get_git_root()) / "src"
+    os.chdir(prj_srcs)  # Needed for fortdepend
+
+    gaussjacobiquad = fortdepend.FortranProject()
+    concat_files = gaussjacobiquad.get_all_used_files(modulename)
+    print(concat_files)
+
+    exp_dat = []
+
+    for idx, fname in enumerate(reversed(concat_files)):
+        processed_content = process_files(prj_srcs / fname, strip_comments)
+        exp_dat.append(processed_content)
+
+    with Path(get_git_root() / "dist" / outname).open("w") as f_out:
+        f_out.write("".join(exp_dat))
+
+
+parser = argparse.ArgumentParser(description="Export single file algorithms.")
+parser.add_argument("--modulename", type=str, required=True, help="Module to export")
+parser.add_argument(
+    "--outname", type=str, required=False, help="Defaults to modulename_single.f90"
+)
+parser.add_argument(
+    "--strip-comments", type=bool, default=True, help="Should comments be stripped?"
+)
+
+args = parser.parse_args()
+export_single(args.modulename, args.strip_comments, args.outname)
